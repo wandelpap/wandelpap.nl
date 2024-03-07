b256ecf4250a4fb397ef7b791d4cdd4c8ae38101
Bas Peeters <bas@peete.rs>
2024-03-07 12:09:54 +0100

    chore: Remove default tag settings

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,14 +10,8 @@ export default {
         {
           children: `window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
-gtag('consent', 'default', {
-  'ad_user_data': 'denied',
-  'ad_personalization': 'denied',
-  'ad_storage': 'denied',
-  'analytics_storage': 'denied',
-  'wait_for_update': 500,
-});
 gtag('js', new Date());
+
 gtag('config', 'G-57CT6TZ3LB');`,
         },
       ],
